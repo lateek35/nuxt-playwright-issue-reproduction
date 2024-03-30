107d7c5ceb22341e3f5234c09bfb946cf46e2f89
Allan Raquin <lateek35@gmail.com>
2024-03-30 19:14:53 +0100

    edit

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,9 +23,9 @@ export default defineConfig<ConfigOptions>({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 1 : 1,
+  retries: 1,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   timeout: 999999999,
